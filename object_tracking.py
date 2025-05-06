fb2ac1170415249d3b376c11b3ebc147733c716c
Sweta Maurya <119293160+sweta-maurya@users.noreply.github.com>
2025-05-06 19:57:12 +0530

    Add files via upload

---
+++ object_tracking.py
@@ -0,0 +1,134 @@
+import cv2
+import numpy as np
+import math
+
+class ObjectDetection:
+    def __init__(self):
+        self.net = cv2.dnn.readNet("D:/Pictures/source_code/dnn_model/yolov4.weights", 
+                                   "D:/Pictures/source_code/dnn_model/yolov4.cfg")
+        self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
+        self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+        self.output_layers = self.net.getUnconnectedOutLayersNames()
+
+    def detect(self, frame):
+        blob = cv2.dnn.blobFromImage(frame, 1/255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+        self.net.setInput(blob)
+        outputs = self.net.forward(self.output_layers)
+        return self.process_outputs(outputs, frame)
+
+    def process_outputs(self, outputs, frame):
+        class_ids = []
+        confidences = []
+        boxes = []
+        h, w = frame.shape[:2]
+        for output in outputs:
+            for detection in output:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:  # Confidence threshold
+                    center_x = int(detection[0] * w)
+                    center_y = int(detection[1] * h)
+                    width = int(detection[2] * w)
+                    height = int(detection[3] * h)
+                    x = int(center_x - width / 2)
+                    y = int(center_y - height / 2)
+                    boxes.append([x, y, width, height])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+        return class_ids, confidences, boxes
+
+cap = cv2.VideoCapture("los_angeles.mp4")
+
+# Initialize count
+count = 0
+center_points_prev_frame = []
+
+tracking_objects = {}
+track_id = 0
+
+od = ObjectDetection()
+
+while True:
+    ret, frame = cap.read()
+    count += 1
+    if not ret:
+        break
+
+    # Point current frame
+    center_points_cur_frame = []
+
+    # Detect objects on frame
+    (class_ids, scores, boxes) = od.detect(frame)
+    for box in boxes:
+        (x, y, w, h) = box
+        cx = int((x + x + w) / 2)
+        cy = int((y + y + h) / 2)
+        center_points_cur_frame.append((cx, cy))
+        #print("FRAME N°", count, " ", x, y, w, h)
+
+        # cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # Only at the beginning we compare previous and current frame
+    if count <= 2:
+        for pt in center_points_cur_frame:
+            for pt2 in center_points_prev_frame:
+                distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
+
+                if distance < 20:
+                    tracking_objects[track_id] = pt
+                    track_id += 1
+    else:
+
+        tracking_objects_copy = tracking_objects.copy()
+        center_points_cur_frame_copy = center_points_cur_frame.copy()
+
+        for object_id, pt2 in tracking_objects_copy.items():
+            object_exists = False
+            for pt in center_points_cur_frame_copy:
+                distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
+
+                # Update IDs position
+                if distance < 20:
+                    tracking_objects[object_id] = pt
+                    object_exists = True
+                    if pt in center_points_cur_frame:
+                        center_points_cur_frame.remove(pt)
+                    continue
+
+            # Remove IDs lost
+            if not object_exists:
+                tracking_objects.pop(object_id)
+
+        # Add new IDs found
+        for pt in center_points_cur_frame:
+            tracking_objects[track_id] = pt
+            track_id += 1
+
+    for object_id, pt in tracking_objects.items():
+        cv2.circle(frame, pt, 5, (0, 0, 255), -1)
+        cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 0, 255), 2)
+
+    print("Tracking objects")
+    print(tracking_objects)
+
+
+    print("CUR FRAME LEFT PTS")
+    print(center_points_cur_frame)
+
+
+    cv2.imshow("Frame", frame)
+
+    # Make a copy of the points
+    center_points_prev_frame = center_points_cur_frame.copy()
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+# import os
+# if not os.path.exists("yolov4.cfg"):
+#     print("Error: yolov4.cfg file not found.")
+#     exit()
